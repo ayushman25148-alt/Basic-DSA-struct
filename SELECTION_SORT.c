3b37d13fc1ddc2b0fb45b06dc895c291db321b37
ayushman25148-dank <ayushman25148@iiitd.ac.in>
2026-03-11 14:08:37 +0530

    Implement selection sort algorithm

---
+++ SELECTION_SORT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selectionsort(int arr[],int n){
+    for(int i=0;i<=n-2;i++){
+        int min = i;
+        for(int j=i+1;j<=n-1;j++){
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
